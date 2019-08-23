64ca41bfc56158f54bc728e7d3d1014645c8c861
Murilo Sergio <murilo.cc.uepb@gmail.com>
2019-08-23 11:37:40 -0300

    Add the DELETE method in sessions

--- src/services/accel-cmd-tcp.js
+++ src/services/accel-cmd-tcp.js
@@ -1,5 +1,5 @@
 net = require('net');
-const HOST = '192.168.0.113';
+const HOST = '127.0.0.1';
 const PORT = 2001;
         
 module.exports={
@@ -31,36 +31,26 @@ module.exports={
                         sessoes = new Array();
                         for(var i=0;i<linhas.length;i++){
                                 var colunas=linhas[i].split("|");
-                                sessao = {
-                                        "ifname": colunas[0].trim(),
-                                        "username": colunas[1].trim(),
-                                        "ip": colunas[2].trim(),
-                                        "ip6": colunas[3].trim(),
-                                        "ip6dp": colunas[4].trim(),
-                                        "type": colunas[5].trim(),
-                                        "uptime": colunas[6].trim(),
-                                        "callingSid": colunas[7].trim(),
-                                        "calledSid": colunas[8].trim(),
-                                        "sid": colunas[9].trim(),
-                                        "rxBytes": colunas[10].trim(),
-                                        "txBytes": colunas[11].trim()
+                                var sessao="{";
+                                for(var w=0;w<colunas.length;w++){
+                                        colunas[w]=colunas[w].trim();
+                                        sessao=sessao+JSON.stringify(cabecalho[w])+": "+JSON.stringify(colunas[w]);
+                                        if(w!=colunas.length-1){
+                                                sessao=sessao+", ";
+                                        }
                                 }
-                                //Adiciona os dados de uma sessao ao array sessoes
-                                sessoes.push(sessao);
+                                sessao=sessao+"}";
+                                var sessionObject=JSON.parse(sessao);
+                                sessoes.push(sessionObject);                                
                         }
         
-                        //Cria objeto para ser retornado na requisicao http
-                        
+                        //Crate object for HTTP connection return                        
                         objeto={totalSessoes: sessoes.length,sessoes: sessoes};
                         retorno=objeto;
-
                         res.status(200).send(retorno);
                         
-                        //Finaliza conexao TCP                
-                        client.destroy();
-
-                        
-                        
+                        //Terminate TCP connection                
+                        client.destroy();                       
                 });
         
                 // Add a 'close' event handler for the client socket
@@ -68,7 +58,26 @@ module.exports={
                 });                  
                 
         }
-}    
+        ,
+        terminateSessionsByInterface: function (terminate_type,terminate_parameter,terminate_mode){
+                console.log(new Date().toLocaleString()+" [info] Send terminate session command");
+                var client = new net.Socket();
+
+                client.connect(PORT, HOST, function() {
+                        // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+                        client.write('terminate '+ terminate_type+" "+terminate_parameter+" "+terminate_mode+'\n');
+                });
+
+                client.on('data', function(data){
+                        console.log(data.toString().trim());
+                });
+                
+                client.on('close', function() {
+                });
+
+        }
+         
+}
 
 
 
